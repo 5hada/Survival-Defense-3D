2895118d53be95c6e359c3dcfc18854e31f6d329
5hada <81249902+5hada@users.noreply.github.com>
2025-06-29 15:25:45 +0900

    1-4
    팔 구현

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public string handName;
+    public float range;
+    public int damage;
+    public float workSpeed;
+    public float attackDelay;
+    public float attackDelayA;
+    public float attackDelayB;
+
+    public Animator anim;
+}
---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    private Hand currentHand;
+
+    private bool isAttack = false;
+    private bool isSwing = false;
+
+    private RaycastHit hitInfo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (!isAttack)
+            {
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        currentHand.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true;
+
+        StartCoroutine(Hitcoroutine());
+
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+        isAttack = false;
+
+    }
+
+    IEnumerator Hitcoroutine()
+    {
+        while(isSwing)
+        {
+            if (CheckObject())
+            {
+                isSwing = false;
+            }
+            yield return null;
+        }
+    }
+
+    private bool CheckObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, currentHand.range))
+        {
+            return true;
+        }
+        return false;
+    }
+}
